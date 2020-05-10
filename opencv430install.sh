0132e46e1af98f6162f4177ad18613d3cdcc55e3
hn-88 <hn-88@users.noreply.github.com>
2020-05-10 13:47:52 +0530

    creating an install script

---
+++ opencv430install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install script for OpenCV-4.3.0
+# using pre-built binaries
+# from https://github.com/hn-88/opencvdeb/
+
+# install all the opencv dependencies
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
+wget https://github.com/hn-88/opencvdeb/releases/download/v4.3.0.1/OpenCVbuild.zip
+unzip OpenCVbuild.zip
+
+mv home/travis/build/hn-88/opencvdeb/opencv/build/OpenCVLocal OpenCVLocal
+rm -Rvf ~/home
+
+
+
