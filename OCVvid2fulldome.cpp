795a6559d5016147a754f8256733805aeb9fa08a
hn-88 <hn-88@users.noreply.github.com>
2020-05-10 13:04:59 +0530

    initial skeleton

---
+++ OCVvid2fulldome.cpp
@@ -0,0 +1,45 @@
+/*
+ * OCVvid2fulldome.cpp
+ * 
+ * Warps "flat" video files to fit 180 degree fisheye "fulldome masters" using the OpenCV framework. 
+ * Appends timestamp to the first filename and saves as default codec (DIVX avi) in the same folder.
+ * 
+ * first commit:
+ * Hari Nandakumar
+ * 10 May 2020
+ * 
+ * 
+ */
+
+//#define _WIN64
+//#define __unix__
+
+// references 
+// http://paulbourke.net/geometry/transformationprojection/
+// equations in figure at http://paulbourke.net/dome/dualfish2sphere/
+// http://paulbourke.net/dome/dualfish2sphere/diagram.pdf
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __unix__
+#include <unistd.h>
+#endif
+
+#include <string.h>
+
+#include <time.h>
+//#include <opencv2/opencv.hpp>
+//#include "tinyfiledialogs.h"
+
+#define CV_PI   3.1415926535897932384626433832795
+
+//using namespace cv;
+
+// some global variables
+
+
+int main(int argc,char *argv[])
+{
+  return 0;
+}
